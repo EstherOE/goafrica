e9ce2d78d4ae3bc63055af00021b6697027c5ce1
estheroe <epelleoesther@gmail.com>
2022-04-03 23:31:03 +0100

    Obstacles almost completed

---
+++ Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public float zSpawn = 0;
+    public float tileLength = 15.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTile(0);
+        SpawnTile(2);
+        SpawnTile(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTile(int titleIndex)
+    {
+
+        Instantiate(prefabs[titleIndex], transform.forward * zSpawn, transform.rotation);
+        zSpawn += tileLength;
+    }
+}
